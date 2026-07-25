87478b632478de88ad4203d466da8f3d2e1f2888
Tanner-Halpin <tannerhalpin@outlook.com>
2026-07-24 23:15:38 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include "hardwareanalysis.h"
 #include <QtWidgets/QApplication>
-#include <qsqldatabase.h>
-#include <qsqlquery.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("PSQL");
-    db.setHostName("localhost");
-    db.setDatabaseName("Hardware_Analysis_DB");
-    db.setUserName("postgres");
-    db.setPassword("cPlusP025!");
-    bool ok = db.open();
 
     HardwareAnalysis window;
     window.show();
