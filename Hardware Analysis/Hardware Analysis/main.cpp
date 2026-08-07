57c5e0105d6b884b28d93e6e4c12610bce575cd4
tanne <tannerhalpin@outlook.com>
2026-08-07 11:54:26 -0400

    Commit All and Push Test

--- Hardware Analysis/Hardware Analysis/main.cpp
+++ Hardware Analysis/Hardware Analysis/main.cpp
@@ -1,10 +1,10 @@
 #include "hardwareanalysis.h"
 #include <QtWidgets/QApplication>
+#include "QtSql/qsqldatabase.h"
 
 int main(int argc, char *argv[])
-{
+{    
     QApplication app(argc, argv);
-
     HardwareAnalysis window;
     window.show();
     return app.exec();
