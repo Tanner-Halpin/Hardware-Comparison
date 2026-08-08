31d784a00d0f8a86d0699a187643285d4018b435
tanne <tannerhalpin@outlook.com>
2026-08-07 22:12:42 -0400

    Comment

--- Hardware Analysis/Hardware Analysis/main.cpp
+++ Hardware Analysis/Hardware Analysis/main.cpp
@@ -1,10 +1,21 @@
 #include "hardwareanalysis.h"
 #include <QtWidgets/QApplication>
+#include <QtSql/qsqldatabase.h>
 
 int main(int argc, char *argv[])
 {    
     QApplication app(argc, argv);
+
     HardwareAnalysis window;
     window.show();
     return app.exec();
 }
+
+/*
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setHostName("bigblue");
+    db.setDatabaseName("flightdb");
+    db.setUserName("acarlson");
+    db.setPassword("1uTbSbAs");
+    bool ok = db.open();
+*/
